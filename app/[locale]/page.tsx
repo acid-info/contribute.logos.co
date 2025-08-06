6caaa3963ed2e3e2cfaaf0b1fb0add84e70b8969
jinhojang6 <jinho5051@gmail.com>
2025-08-07 03:02:23 +0900

    feat: support i18n

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import ThemeToggle from '@/components/theme-toggle'
 import { ROUTES } from '@/constants/routes'
 import { createDefaultMetadata } from '@/utils/metadata'
+import { useTranslations } from 'next-intl'
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params
@@ -15,12 +16,13 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: s
   return metadata
 }
 
-export default async function Page() {
+export default function Page() {
+  const t = useTranslations('home')
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
         <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-          Home Page
+          {t('title')}
         </h1>
         <br />
         <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">Description</p>
--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+const createNextIntlPlugin = require('next-intl/plugin')
+
+const withNextIntl = createNextIntlPlugin()
 
 // You might need to insert additional domains in script-src if you are using external services
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
-  connect-src *;
-  font-src 'self';
-  frame-src giscus.app
-`
 
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: 'Referrer-Policy',
@@ -61,8 +45,7 @@ const unoptimized = process.env.UNOPTIMIZED ? true : undefined
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 module.exports = () => {
-  const plugins = [withBundleAnalyzer]
-  return plugins.reduce((acc, next) => next(acc), {
+  return withNextIntl({
     // for static builds
     // output: 'export',
     output,
@@ -74,12 +57,6 @@ module.exports = () => {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-        },
-      ],
       unoptimized,
     },
     async headers() {
