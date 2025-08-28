381dfa94e4fe5694d7060582381daa4aa9185a49
JulesFILIOT <dev@julesfiliot.com>
2025-08-28 12:35:36 +0900

    chore(api): update api url

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,5 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getContributeApiBase() {
-  return 'https://dev-admin-acid.logos.co/api'
+  return 'https://logos-admin-git-develop-acidinfo.vercel.app/api'
 }
