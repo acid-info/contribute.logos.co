ae5cdb9333b63da9cec6b64abcb8169e35f1e255
Xavier Saliniere <bonjour@xaviers.sh>
2025-09-19 11:40:34 -0400

    refactor: add formatDate to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import clsx, { ClassValue } from 'clsx'
+import clsx, { type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -22,4 +22,12 @@ export function getContributeApiBase() {
 export function formatNumber(num: number | undefined | null): string {
   if (num == null) return '0'
   return num.toLocaleString()
+}
+
+export function formatDate(dateString: string): string {
+  return new Date(dateString).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
 }
