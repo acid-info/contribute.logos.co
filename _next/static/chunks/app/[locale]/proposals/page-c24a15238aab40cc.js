451dbbc1081cce545d798fe7aedf496c77718f62
status-im-auto <auto@status.im>
2026-01-14 09:36:45 +0000

    Update documentation

---
+++ _next/static/chunks/app/[locale]/proposals/page-c24a15238aab40cc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[273],{1989:(e,s,_)=>{Promise.resolve().then(_.bind(_,5153))}},e=>{var s=s=>e(e.s=s);e.O(0,[55,700,460,941,509,153,969,603,358],()=>s(1989)),_N_E=e.O()}]);
